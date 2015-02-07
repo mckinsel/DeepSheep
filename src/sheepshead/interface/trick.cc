991cbcb7ba07f9067f33bd1f1d12ed3283b8a50c
Marcus Kinsella <marcus.kinsella@gmail.com>
2015-02-07 10:48:22 -0800

    Add methods that return iterators over laid cards.

--- src/sheepshead/interface/trick.cc
+++ src/sheepshead/interface/trick.cc
@@ -12,23 +12,6 @@ Trick<Handle_T>::Trick()
   : m_hand_ptr(nullptr), m_trick_number(-1)
 {}
 
-
-template<typename Handle_T>
-bool Trick<Handle_T>::is_started() const
-{
-  if(m_trick_number < 0)
-    return false;
-
-  if(m_hand_ptr->tricks_size() <= m_trick_number)
-    return false;
-  
-  auto model_trick = m_hand_ptr->tricks(m_trick_number);
-  if(model_trick.laid_cards_size() == 0)
-    return false;
-
-  return true;
-}
-
 template<typename Handle_T>
 bool Trick<Handle_T>::is_null() const
 {
@@ -36,9 +19,21 @@ bool Trick<Handle_T>::is_null() const
 }
 
 template<typename Handle_T>
+bool Trick<Handle_T>::is_started() const
+{
+  if(this->is_null()) return false;
+
+  if(m_hand_ptr->tricks_size() <= m_trick_number)
+    return false;
+  
+  return m_hand_ptr->tricks_size() > m_trick_number;
+}
+
+
+template<typename Handle_T>
 bool Trick<Handle_T>::is_finished() const
 {
-  if(!this->is_started())
+  if(!this->is_started()) // Checks is_null
     return false;
 
   auto model_trick = m_hand_ptr->tricks(m_trick_number);
@@ -47,19 +42,44 @@ bool Trick<Handle_T>::is_finished() const
   return model_trick.laid_cards_size() == rules.number_of_players();
 }
 
-template class Trick<ConstHandHandle>;
-template class Trick<MutableHandHandle>;
-
-/*PlayerId Trick::leader() const
+template<typename Handle_T>
+PlayerItr Trick<Handle_T>::leader() const
 {
-  if(m_hand_ptr->tricks_size() <= m_trick_number)
-    return PlayerId();
+  if(!this->is_started()) return PlayerItr(); // Checks is_null
 
   auto model_trick = m_hand_ptr->tricks(m_trick_number);
-  return PlayerId(m_hand_ptr, model_trick.leader_position());
+  return PlayerItr(m_hand_ptr, model_trick.leader_position());
 }
 
-PlayerId Trick::winner() const
+template<typename Handle_T>
+CardItr Trick<Handle_T>::laid_cards_begin() const
+{
+  if(!this->is_started()) return CardItr();
+
+  auto&& laid_cards = m_hand_ptr->tricks(m_trick_number)
+                      .laid_cards();
+  return CardItr(m_hand_ptr, laid_cards.begin());
+}
+
+template<typename Handle_T>
+CardItr Trick<Handle_T>::laid_cards_end() const
+{
+  if(!this->is_started()) return CardItr();
+
+  auto&& laid_cards = m_hand_ptr->tricks(m_trick_number)
+                      .laid_cards();
+  return CardItr(m_hand_ptr, laid_cards.end());
+}
+
+template<typename Handle_T>
+int Trick<Handle_T>::number_of_laid_cards() const
+{
+  if(!this->is_started()) return 0;
+
+  return m_hand_ptr->tricks(m_trick_number).laid_cards_size();
+}
+
+/*PlayerId Trick::winner() const
 {
   if(!(this->is_finished())) return PlayerId();
   
@@ -67,6 +87,9 @@ PlayerId Trick::winner() const
   return PlayerId();
 }*/
 
+
+template class Trick<ConstHandHandle>;
+template class Trick<MutableHandHandle>;
 
 //TrickItr
 template<typename Handle_T>
@@ -110,9 +133,16 @@ TrickItr<Handle_T> TrickItr<Handle_T>::operator--(int)
 } 
 
 template<typename Handle_T>
+bool TrickItr<Handle_T>::is_null() const
+{
+  return m_hand_ptr == nullptr;
+}
+
+template<typename Handle_T>
 Trick<Handle_T>& TrickItr<Handle_T>::operator*()
 {
-  if(m_trick_number < 0 ||
+  if(this->is_null()    ||
+     m_trick_number < 0 ||
      m_trick_number >= m_hand_ptr->tricks_size())
   {
     m_trick = Trick<Handle_T>();
@@ -121,6 +151,12 @@ Trick<Handle_T>& TrickItr<Handle_T>::operator*()
   }
 
   return m_trick;
+}
+
+template<typename Handle_T>
+Trick<Handle_T>* TrickItr<Handle_T>::operator->()
+{
+  return &(operator*());
 }
 
 template<typename Handle_T>
--- src/sheepshead/interface/trick.h
+++ src/sheepshead/interface/trick.h
@@ -4,7 +4,8 @@
 #include "sheepshead/proto/game.pb.h"
 
 #include "sheepshead/interface/handle_types.h"
-//#include "sheepshead/interface/playerid.h"
+#include "sheepshead/interface/playerid.h"
+#include "sheepshead/interface/deck.h"
 
 #include <iterator>
 #include <memory>
@@ -28,20 +29,28 @@ public:
   Trick(const Handle_T& hand_ptr, int trick_number) 
     : m_hand_ptr(hand_ptr), m_trick_number(trick_number) {}
 
-  //! Return true  if the Trick wraps a null trick, a conceptually non-existent trick.
+  //! Return true if the Trick wraps a null trick, a conceptually non-existent trick.
   bool is_null() const;
   
   /// Return true if the Trick has started.
 
-  //! Even if no cards for the trick have been played yet, will return true if
-  //! the trick has been created in the model.
+  //! Even if no cards for the trick have been played yet, will
+  //! return true if the trick has been created in the model.
   bool is_started() const; 
 
   //! Return true if all cards for the Trick have been played.
   bool is_finished() const;
   
-  //! Return the PlayerId of the player who plays the first card of the Trick.
-  //PlayerId leader() const;
+  //! Return an iterator pointing to the player who led.
+  PlayerItr leader() const;
+
+  //! Return an iterator pointing to the first card played in the trick.
+  CardItr laid_cards_begin() const;
+
+  //! Return an iterator pointing past the end of cards played in the trick.
+  CardItr laid_cards_end() const;
+
+  int number_of_laid_cards() const;
 
   //! Return the PlayerId of the player who won the Trick.
 
@@ -60,7 +69,8 @@ private:
 /// An iterator over the Tricks of a Hand History.
 template<typename Handle_T>
 class TrickItr
-  : public std::iterator<std::forward_iterator_tag, Trick<Handle_T>, int>
+  : public std::iterator<std::bidirectional_iterator_tag,
+                         Trick<Handle_T>, int>
 {
 public:
   TrickItr();
@@ -69,12 +79,15 @@ public:
 
   TrickItr(const Handle_T& hand_ptr, int trick_number);
   
+  bool is_null() const;
+
   TrickItr& operator++();
   TrickItr operator++(int);
   TrickItr& operator--();
   TrickItr operator--(int);
 
   Trick<Handle_T>& operator*();
+  Trick<Handle_T>* operator->();
 
   bool operator==(const TrickItr& rhs) const;
   bool operator!=(const TrickItr& rhs) const;
