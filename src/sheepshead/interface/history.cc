cd2e90774c2c78a1dfdfbf49fb525a9307a7f810
Marcus Kinsella <marcus.kinsella@gmail.com>
2015-02-07 10:31:35 -0800

    Add method for latest Trick.

--- src/sheepshead/interface/history.cc
+++ src/sheepshead/interface/history.cc
@@ -4,7 +4,6 @@
 namespace sheepshead {
 namespace interface {
 
-template <typename T> class TD;
 
 History::History(const ConstHandHandle& hand_ptr)
   : m_hand_ptr(hand_ptr)
@@ -16,13 +15,13 @@ PickingRound<decltype(History::m_hand_ptr)> History::picking_round() const
 }
 
 
-TrickItr<decltype(History::m_hand_ptr)> History::tricks_cbegin() const
+TrickItr<decltype(History::m_hand_ptr)> History::tricks_begin() const
 {
 
   return TrickItr<decltype(m_hand_ptr)>(m_hand_ptr, 0);
 }
 
-TrickItr<decltype(History::m_hand_ptr)> History::tricks_cend() const
+TrickItr<decltype(History::m_hand_ptr)> History::tricks_end() const
 {
   return TrickItr<decltype(m_hand_ptr)>(m_hand_ptr, m_hand_ptr->tricks_size());
 }
@@ -30,6 +29,22 @@ TrickItr<decltype(History::m_hand_ptr)> History::tricks_cend() const
 int History::number_of_started_tricks() const
 {
   return m_hand_ptr->tricks_size();
+}
+
+int History::number_of_finished_tricks() const
+{
+  return std::count_if(this->tricks_begin(), this->tricks_end(),
+                       [](const Trick<decltype(m_hand_ptr)>& t)
+                         {return t.is_finished();});
+}
+
+Trick<decltype(History::m_hand_ptr)> History::latest_trick() const
+{
+  int num_started = this->number_of_started_tricks();
+
+  if(num_started == 0) return Trick<decltype(m_hand_ptr)>();
+
+  return Trick<decltype(m_hand_ptr)>(m_hand_ptr, num_started);
 }
 
 } // namespace interface
--- src/sheepshead/interface/history.h
+++ src/sheepshead/interface/history.h
@@ -22,13 +22,16 @@ public:
   History(const ConstHandHandle& hand_ptr); 
   
   //! An interface to the Hand's picking round 
-  PickingRound<decltype(m_hand_ptr)> picking_round() const;
+  PickingRound<decltype(m_hand_ptr)>
+  picking_round() const;
   
   //! An iterator pointing to the first trick of the Hand.
-  TrickItr<decltype(m_hand_ptr)> tricks_cbegin() const;
+  TrickItr<decltype(m_hand_ptr)>
+  tricks_begin() const;
 
   //! An iterator pointing past the last trick of the Hand.
-  TrickItr<decltype(m_hand_ptr)> tricks_cend() const;
+  TrickItr<decltype(m_hand_ptr)>
+  tricks_end() const;
   
   //! The number of started tricks in the Hand
   int number_of_started_tricks() const;
@@ -37,7 +40,8 @@ public:
   int number_of_finished_tricks() const;
 
   //! The latest Trick being played in the Hand.
-  Trick<decltype(m_hand_ptr)> latest_trick() const;
+  Trick<decltype(m_hand_ptr)>
+  latest_trick() const;
 
 
 }; // class History
