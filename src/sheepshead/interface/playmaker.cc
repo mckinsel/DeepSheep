01528548729b7b59f31d876e7cefff6a54c01467
Marcus Kinsella <marcus.kinsella@gmail.com>
2015-02-07 10:46:47 -0800

    Add unknown and discard plays.

--- src/sheepshead/interface/playmaker.cc
+++ src/sheepshead/interface/playmaker.cc
@@ -1,35 +1,104 @@
 #include "playmaker.h"
+
+#include "handstate.h"
+
 #include <cassert>
 
 namespace sheepshead {
 namespace interface {
 
-Play::Play(PlayType play_tag, model::PickingRound_PickingDecision decision)
+// Play
+Play::Play(PlayType play_tag, PickDecision decision)
   :  m_play_tag(play_tag), m_pick_decision(decision)
 {
   assert(m_play_tag == PlayType::PICK);
 }
 
-Play::Play(PlayType play_tag, model::PickingRound_LonerDecision decision)
+Play::Play(PlayType play_tag, LonerDecision decision)
   :  m_play_tag(play_tag), m_loner_decision(decision)
 {
   assert(m_play_tag == PlayType::LONER);
 }
 
-Play::Play(PlayType play_tag, model::Card decision)
+Play::Play(PlayType play_tag, Card&& decision)
   :  m_play_tag(play_tag), m_card_decision(decision)
 {
   assert(m_play_tag == PlayType::PARTNER ||
          m_play_tag == PlayType::TRICK_CARD);
 }
 
+Play::Play(PlayType play_tag, std::pair<Card, Card::Suit>&& decision)
+  : m_play_tag(play_tag), m_unknown_decision(decision)
+{
+  assert(m_play_tag == PlayType::UNKNOWN);
+}
+
+Play::Play(PlayType play_tag, std::vector<Card>&& decision)
+  : m_play_tag(play_tag), m_discard_decision(decision)
+{
+  assert(m_play_tag == PlayType::DISCARD);
+}
+
 Play::~Play()
 {
   // A model::Card is not trivially destructible, so if that's what we've got,
   // we have to call its destructor.
-  if(m_play_tag == PlayType::PARTNER || m_play_tag == PlayType::TRICK_CARD) {
+  if(m_play_tag == PlayType::UNKNOWN) {
+    m_unknown_decision.~pair<Card, Card::Suit>();
+  } else if(m_play_tag == PlayType::DISCARD) {
+    m_discard_decision.~vector<Card>();
+  } else if(m_play_tag == PlayType::PARTNER ||
+            m_play_tag == PlayType::TRICK_CARD) {
     m_card_decision.~Card();
   }
+}
+
+Play::Play(const Play& from)
+: m_play_tag(from.m_play_tag)
+{
+  switch(m_play_tag) {
+    case PlayType::PICK :
+      m_pick_decision = from.m_pick_decision;
+      break;
+    case PlayType::LONER :
+      m_loner_decision = from.m_loner_decision;
+      break;
+    case PlayType::PARTNER :
+    case PlayType::TRICK_CARD :
+      m_card_decision = from.m_card_decision;
+      break;
+    case PlayType::DISCARD :
+      m_discard_decision = from.m_discard_decision;
+      break;
+    case PlayType::UNKNOWN :
+      m_unknown_decision = from.m_unknown_decision;
+      break;
+  }
+}
+
+Play& Play::operator=(const Play& from)
+{
+  m_play_tag = from.m_play_tag;
+  switch(m_play_tag) {
+    case PlayType::PICK :
+      m_pick_decision = from.m_pick_decision;
+      break;
+    case PlayType::LONER :
+      m_loner_decision = from.m_loner_decision;
+      break;
+    case PlayType::PARTNER :
+    case PlayType::TRICK_CARD :
+      m_card_decision = from.m_card_decision;
+      break;
+    case PlayType::DISCARD :
+      m_discard_decision = from.m_discard_decision;
+      break;
+    case PlayType::UNKNOWN :
+      m_unknown_decision = from.m_unknown_decision;
+      break;
+  }
+
+  return *this;
 }
 
 bool Play::operator==(const Play& rhs) const
@@ -46,8 +115,20 @@ bool Play::operator==(const Play& rhs) const
       break;
     case PlayType::PARTNER :
     case PlayType::TRICK_CARD :
-      return m_card_decision.suit() == rhs.m_card_decision.suit() &&
-             m_card_decision.rank() == rhs.m_card_decision.rank();
+      return m_card_decision == rhs.m_card_decision;
+      break;
+    case PlayType::DISCARD :
+      if(m_discard_decision.size() !=
+          rhs.m_discard_decision.size())
+        return false;
+      for(size_t i=0; i < m_discard_decision.size(); i++) {
+        if(m_discard_decision[i] != rhs.m_discard_decision[i])
+          return false;
+      }
+      return true;
+      break;
+    case PlayType::UNKNOWN :
+      return m_unknown_decision == rhs.m_unknown_decision;
       break;
   }
 }
@@ -57,6 +138,10 @@ bool Play::operator!=(const Play& rhs) const
   return !(*this == rhs);
 }
 
+// end Play
+
+// Playmaker
+
 Playmaker::Playmaker(const MutableHandHandle& hand_ptr, const PlayerId& playerid)
   : m_hand_ptr(hand_ptr), m_playerid(playerid)
 {}
@@ -64,6 +149,15 @@ Playmaker::Playmaker(const MutableHandHandle& hand_ptr, const PlayerId& playerid
 std::vector<Play> Playmaker::available_plays() const
 {
   std::vector<Play> plays;
+
+  // If the hand cannot be played, just return an empty vector.
+  if(!internal::is_playable(m_hand_ptr)) return plays;
+
+  if(internal::ready_for_pick_play(m_hand_ptr) == m_playerid) {
+    plays.emplace_back(Play::PlayType::PICK, PickDecision::PICK);
+    plays.emplace_back(Play::PlayType::PICK, PickDecision::PASS);
+    return plays;
+  }
 
   return plays;
 }
--- src/sheepshead/interface/playmaker.h
+++ src/sheepshead/interface/playmaker.h
@@ -4,42 +4,58 @@
 #include "sheepshead/proto/game.pb.h"
 #include "sheepshead/interface/handle_types.h"
 #include "sheepshead/interface/playerid.h"
+#include "sheepshead/interface/deck.h"
+#include "sheepshead/interface/pickinground.h"
 
 #include <vector>
 
 namespace sheepshead {
 namespace interface {
 
-/// Enum for the different types of play that a player can make.
 
 /// Represents a single possible play for a Hand.
 class Play
 {
 public:
-  // Explicit destructor to handle destruction of variant type.
+  // Enum for the different types of play that a player can make.
+  enum class PlayType {PICK, LONER, PARTNER, UNKNOWN, DISCARD, TRICK_CARD};
+
+  // Constructors for the different Play types.
+  // PICK
+  Play(PlayType play_tag, PickDecision decision);
+  // LONER
+  Play(PlayType play_tag, LonerDecision decision);
+  // PARTNER, TRICK_CARD
+  Play(PlayType play_tag, Card&& decision);
+  // UNKNOWN
+  Play(PlayType play_tag, std::pair<Card, Card::Suit>&& decision);
+  // DISCARD
+  Play(PlayType play_tag, std::vector<Card>&& decision);
+
+  // These all need to be explicit because of the variant type.
   virtual ~Play();
+  Play(const Play& from);
+  Play& operator=(const Play& from);
+  Play& operator=(Play&& from);
+  Play(Play&& from);
 
   bool operator==(const Play& rhs) const;
   bool operator!=(const Play& rhs) const;
 
 private:
-  enum class PlayType {PICK, LONER, PARTNER, TRICK_CARD};
 
-  Play(PlayType play_tag, model::PickingRound_PickingDecision decision);
-  Play(PlayType play_tag, model::PickingRound_LonerDecision decision);
-  Play(PlayType play_tag, model::Card decision);
-  //Play(PlayTupe play_tag, std::tuple<model::Card, model::Suit> decision);
-  //Play(PlayType play_tag, std::vector<model::Card> decision);
-
-  const PlayType m_play_tag;
-  const union {
-    model::PickingRound_PickingDecision m_pick_decision;
-    model::PickingRound_LonerDecision m_loner_decision;
-    model::Card m_card_decision;
+  PlayType m_play_tag;
+  union {
+    PickDecision m_pick_decision;
+    LonerDecision m_loner_decision;
+    Card m_card_decision;
+    std::pair<Card, Card::Suit> m_unknown_decision;
+    std::vector<Card> m_discard_decision;
   };
 
 }; // class Play
 
+/// The interface to altering Hand by having a player make plays.
 class Playmaker
 {
 public:
@@ -49,8 +65,8 @@ public:
 private:
   friend class Hand;
   Playmaker(const MutableHandHandle& hand_ptr, const PlayerId& playerid);
-  const MutableHandHandle m_hand_ptr;
-  const PlayerId m_playerid;
+  MutableHandHandle m_hand_ptr;
+  PlayerId m_playerid;
 
 }; // class Playmaker
 
