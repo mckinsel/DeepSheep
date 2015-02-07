2439dbc81ae19679aa2883e6268fb5ef0aa32c0c
Marcus Kinsella <marcus.kinsella@gmail.com>
2015-02-07 10:54:05 -0800

    Add gtest boilerplate.

--- tests/proto/test.cc
+++ tests/proto/test.cc
@@ -5,5 +5,11 @@ TEST(TestModelHand, TestEmptyOnInit)
 {
   auto hand = sheepshead::model::Hand();
   EXPECT_EQ(hand.tricks_size(), 0);
+}
 
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  auto results = RUN_ALL_TESTS();
+  google::protobuf::ShutdownProtobufLibrary();
+  return results;
 }
