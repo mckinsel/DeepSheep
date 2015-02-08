fe453528cafa71b44387e5961540a757eb0c26d6
Marcus Kinsella <marcus.kinsella@gmail.com>
2015-02-07 16:23:25 -0800

    Add tests for the picking round for different rule variations.

--- tests/interface/playmaker_test.cc
+++ tests/interface/playmaker_test.cc
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include "sheepshead/interface/hand.h"
 
+#include <iostream>
+
 // Test that upon initialization, only one player has available plays and that
 // those plays are pick or pass
 TEST(TestPlaymaker, TestPickPassAvailable)
@@ -42,15 +44,63 @@ TEST(TestPlaymaker, TestPickPassAvailable)
   EXPECT_EQ(counter, 5);
 }
 
-// Test that the hand become unplayable when everyone passes
-TEST(TestPlaymaker, TestAllPass)
+void choose_all_pass(sheepshead::interface::Hand* hand)
 {
-  auto hand = sheepshead::interface::Hand();
-  hand.arbiter().arbitrate(); // Initialize the hand
-  EXPECT_TRUE(hand.is_playable());
-
-  auto player_itr = hand.history().picking_round().leader();
+  hand->arbiter().arbitrate();
+  EXPECT_TRUE(hand->is_playable());
+  auto player_itr = hand->history().picking_round().leader();
   auto leader = *player_itr;
+  do {
+    auto available_plays = hand->playmaker(*player_itr).available_plays();
+    EXPECT_EQ(available_plays.size(), 2);
+
+    auto pass_play = sheepshead::interface::Play(
+                          sheepshead::interface::Play::PlayType::PICK,
+                          sheepshead::interface::PickDecision::PASS);
+    EXPECT_TRUE(hand->playmaker(*player_itr).make_play(pass_play));
+    ++player_itr;
+  } while(*player_itr != leader);
+}
+
+// Test that the hand become unplayable when everyone passes
+TEST(TestPlaymaker, TestAllPassLeasters)
+{
+  auto mutable_rules = sheepshead::interface::MutableRules();
+  mutable_rules.set_no_picker_leasters();
+  auto hand = sheepshead::interface::Hand(mutable_rules.get_rules());
+
+  choose_all_pass(&hand);
+
+  EXPECT_TRUE(hand.history().picking_round().is_finished());
+  EXPECT_FALSE(hand.is_playable());
+  EXPECT_FALSE(hand.is_finished());
+  EXPECT_TRUE(hand.is_arbitrable());
+}
+
+TEST(TestPlaymaker, TestAllPassDoubler)
+{
+  auto mutable_rules = sheepshead::interface::MutableRules();
+  mutable_rules.set_no_picker_doubler();
+  auto hand = sheepshead::interface::Hand(mutable_rules.get_rules());
+
+  choose_all_pass(&hand);
+
+  EXPECT_TRUE(hand.history().picking_round().is_finished());
+  EXPECT_FALSE(hand.is_playable());
+  EXPECT_TRUE(hand.is_finished());
+  EXPECT_FALSE(hand.is_arbitrable());
+}
+
+TEST(TestPlaymaker, TestForcedPick)
+{
+  auto mutable_rules = sheepshead::interface::MutableRules();
+  mutable_rules.set_no_picker_forced_pick();
+  auto hand = sheepshead::interface::Hand(mutable_rules.get_rules());
+
+  hand.arbiter().arbitrate();
+  EXPECT_TRUE(hand.is_playable());
+  auto player_itr = hand.history().picking_round().leader();
+  auto last_picker = *std::prev(player_itr);
   do {
     auto available_plays = hand.playmaker(*player_itr).available_plays();
     EXPECT_EQ(available_plays.size(), 2);
@@ -60,12 +110,12 @@ TEST(TestPlaymaker, TestAllPass)
                           sheepshead::interface::PickDecision::PASS);
     EXPECT_TRUE(hand.playmaker(*player_itr).make_play(pass_play));
     ++player_itr;
-  } while(*player_itr != leader);
-  
-  EXPECT_TRUE(hand.history().picking_round().is_finished());
-  EXPECT_FALSE(hand.is_playable());
-  EXPECT_FALSE(hand.is_finished());
-  EXPECT_TRUE(hand.is_arbitrable());
+  } while(*player_itr != last_picker);
+
+  auto available_plays = hand.playmaker(last_picker).available_plays();
+  EXPECT_EQ(available_plays.size(), 1);
+  EXPECT_EQ(*(available_plays[0].pick_decision()),
+            sheepshead::interface::PickDecision::PICK);
 }
 
 int main(int argc, char **argv) {
