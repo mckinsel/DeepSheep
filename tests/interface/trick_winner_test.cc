6e07d874aae9e477048dedacbd8eaeae8cef6e9a
Marcus Kinsella <marcus.kinsella@gmail.com>
2015-03-20 19:26:04 -0700

    Add list of tests to create for trick winner.

---
+++ tests/interface/trick_winner_test.cc
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include "test_plays.h"
+#include "sheepshead/interface/hand.h"
+
+// TODO: Test trump always wins
+// TODO: Test led suit is correct, including trump
+// TODO: Test not following suit fails
+// TODO: Test trump order
+// TODO: Test fail order
