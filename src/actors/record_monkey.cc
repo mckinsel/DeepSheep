df3592e3a0c670c2922dc69da88f76678fd476f3
Marcus Kinsella <marcus.kinsella@gmail.com>
2015-11-28 09:07:57 -0800

    Let random player specify a seed

--- src/actors/record_monkey.cc
+++ src/actors/record_monkey.cc
@@ -1,21 +1,22 @@
 #include "sheepshead/interface/hand.h"
 
 #include <chrono>
+#include <iostream>
 #include <random>
 
 /*
  * Attempt to take a turn by finding player that has some available plays
  * and randomly choosing one of them.
  */
-bool try_to_play(sheepshead::interface::Hand* hand)
+bool try_to_play(sheepshead::interface::Hand* hand, std::default_random_engine& generator)
 {
   std::cerr << "\n*******\nTrying to play " << std::endl;
   auto leader_itr = hand->history().picking_round().leader();
   auto player_itr = leader_itr;
 
 
-  std::default_random_engine generator(std::chrono::system_clock::
-                                       now().time_since_epoch().count());
+  //std::default_random_engine generator(std::chrono::system_clock::
+  //                                     now().time_since_epoch().count());
 
   do {
     auto available_plays = hand->playmaker(*player_itr).available_plays();
@@ -33,14 +34,14 @@ bool try_to_play(sheepshead::interface::Hand* hand)
 
       std::uniform_int_distribution<int>
         distribution(0, available_plays.size() - 1);
-            
+
       unsigned int chosen_play = distribution(generator);
-      
+
       std::cerr << std::endl; 
       std::cerr << "Chosen play is " << available_plays[chosen_play].debug_string() << std::endl;
 
       hand->playmaker(*player_itr).make_play(available_plays[chosen_play]);
-      return true; 
+      return true;
     }
 
     ++player_itr;
@@ -49,10 +50,19 @@ bool try_to_play(sheepshead::interface::Hand* hand)
   return false;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-  auto hand = sheepshead::interface::Hand();
 
+  unsigned long seed = 0;
+  if(argc > 1) {
+    seed = strtoul(argv[1], NULL, 0);
+  } else {
+    seed = std::chrono::system_clock::now().time_since_epoch().count();
+  }
+
+  auto hand = sheepshead::interface::Hand(seed);
+
+  std::default_random_engine generator(seed);
   while(!hand.is_finished()) {
     if(hand.is_arbitrable()) {
       std::cerr << "Arbitrating" << std::endl;
@@ -61,7 +71,7 @@ int main()
     }
 
     if(hand.is_playable()) {
-      bool result = try_to_play(&hand);
+      bool result = try_to_play(&hand, generator);
       if(result) std::cerr  << "Successfully made a play" << std::endl;
     }
   }
