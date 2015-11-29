1e06dad9cc83f86182731022a771dd8c6895d58a
Marcus Kinsella <marcus.kinsella@gmail.com>
2015-11-28 19:56:07 -0800

    Get one random number before starting the game
    Otherwise, the first number always seems to be zero, no matter the seed. This
    makes the first player always pick, which is not what we're going for.

--- src/actors/record_monkey.cc
+++ src/actors/record_monkey.cc
@@ -15,15 +15,11 @@ bool try_to_play(sheepshead::interface::Hand* hand, std::default_random_engine& 
   auto player_itr = leader_itr;
 
 
-  //std::default_random_engine generator(std::chrono::system_clock::
-  //                                     now().time_since_epoch().count());
-
   do {
     auto available_plays = hand->playmaker(*player_itr).available_plays();
 
 
     if(available_plays.size() > 0) {
-      //std::cerr  << hand->history().debug_string() << std::endl;
       std::cerr<< hand->debug_string() << std::endl;
       std::cerr << player_itr->debug_string();
       std::cerr << " has " << available_plays.size() << " available plays." << std::endl;
@@ -31,13 +27,13 @@ bool try_to_play(sheepshead::interface::Hand* hand, std::default_random_engine& 
       for(auto& available_play : available_plays) {
         std::cerr << available_play.debug_string() << "; ";
       }
+      std::cerr << std::endl;
 
       std::uniform_int_distribution<int>
         distribution(0, available_plays.size() - 1);
 
       unsigned int chosen_play = distribution(generator);
 
-      std::cerr << std::endl; 
       std::cerr << "Chosen play is " << available_plays[chosen_play].debug_string() << std::endl;
 
       hand->playmaker(*player_itr).make_play(available_plays[chosen_play]);
@@ -59,10 +55,12 @@ int main(int argc, char* argv[])
   } else {
     seed = std::chrono::system_clock::now().time_since_epoch().count();
   }
-
   auto hand = sheepshead::interface::Hand(seed);
 
   std::default_random_engine generator(seed);
+  std::uniform_int_distribution<int> distribution(0, 2);
+  distribution(generator);
+
   while(!hand.is_finished()) {
     if(hand.is_arbitrable()) {
       std::cerr << "Arbitrating" << std::endl;
